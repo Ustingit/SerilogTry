c1e988da3155a73cf9cfe335ccb59bd7feb0eb4c
yuryi.ustinovich <ustinhtc@gmail.com>
2022-05-13 15:25:16 +0300

    add serilog packages and setting it up

--- SerilogLearning2Api/Program.cs
+++ SerilogLearning2Api/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace SerilogLearning2Api
 {
@@ -18,6 +20,22 @@ namespace SerilogLearning2Api
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
+				.UseSerilog((context, configuration) =>
+				{
+					configuration.Enrich.FromLogContext()
+						.Enrich.WithMachineName()
+						.WriteTo.Console()
+						.WriteTo.Elasticsearch(
+							new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+							{
+								IndexFormat = $"{context.Configuration["ApplicationName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace('.', '-')}-{DateTime.UtcNow:yyyy-MM}",
+								NumberOfShards = 2,
+								NumberOfReplicas = 1,
+								AutoRegisterTemplate = true
+							})
+						.Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+						.ReadFrom.Configuration(context.Configuration);
+				})
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
